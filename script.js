30516f66ce19a5b64cf22c8a7a9f1a11f1719eb7
𝐌7𝐃-𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 <moll71738@gmail.com>
2025-04-17 03:49:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// تحميل الموقع مع شاشة الترحيب
-window.onload = () => {
-    const platforms = document.querySelectorAll('.platform');
-
-    // إزالة شاشة الترحيب بعد انتهاء العرض
-    setTimeout(() => {
-        document.getElementById("welcome-screen").style.display = "none";
-    }, 5500); // 5.5 ثواني بعد اكتمال التأثير
-};
